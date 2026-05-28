ec79cbdbbd1d6c06e178c37ba2a5ac5bd8dcb202
Pedro BG <pedro@dataperu.pe>
2026-05-28 10:40:51 -0500

    security: allow any secure https URL in CSP img-src to support external images in news

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,7 +43,7 @@ export async function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com https://pagead2.googlesyndication.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://*.supabase.co https://*.wp.com https://*.google.com;
+    img-src 'self' blob: data: https:;
     font-src 'self' https://fonts.gstatic.com;
     frame-src 'self' https://challenges.cloudflare.com https://*.google.com;
     connect-src 'self' https://*.supabase.co https://*.googleapis.com https://*.upstash.io https://pagead2.googlesyndication.com;
